ac20b22cde2b146edf94175c1e9577d8cd5b57fd
Bl4ck_Schnitzel <104429747+NewGamez@users.noreply.github.com>
2026-02-23 13:45:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const getAccounts = () => JSON.parse(localStorage.getItem("bs_accounts")) || {
 };
 const saveAccounts = (accs) => localStorage.setItem("bs_accounts", JSON.stringify(accs));
 
-const getAbsences = () => JSON.parse(localStorage.getItem("bs_absences")) || [];
-const saveAbsences = (data) => localStorage.setItem("bs_absences", JSON.stringify(data));
-
-/* --- AUTH & LOGOUT --- */
+/* --- AUTH-LOGIK --- */
 const requireLogin = () => {
     if (!sessionStorage.getItem("loggedInUser")) {
         window.location.href = "login.html";
@@ -19,8 +16,15 @@ const logout = () => {
     window.location.href = "login.html";
 };
 
-/* --- MANAGEMENT FUNKTIONEN --- */
-function isAdmin() {
-    const user = getAccounts()[sessionStorage.getItem("loggedInUser")];
+const isAdmin = () => {
+    const username = sessionStorage.getItem("loggedInUser");
+    const accs = getAccounts();
+    const user = accs[username];
     return user && (user.role === "Management" || user.role === "Cheffe");
-}
+};
+
+const setHeaderUser = () => {
+    const user = sessionStorage.getItem("loggedInUser");
+    const el = document.getElementById("welcomeUser");
+    if (el && user) el.innerText = `Willkommen, ${user}`;
+};
